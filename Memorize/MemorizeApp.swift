adcf575af559878d8a98c0d2a0197355aecfe0a2
Henrique Batista <henriquefreitasbatista.devoteam@edp.com>
2025-04-22 00:44:58 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Henrique Batista on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
