87b6b14a899ea0e9ffbd2dd90e7ab94ec938d261
Henrique Batista <henriquefreitasbatista.devoteam@edp.com>
2025-04-22 02:39:42 +0100

    Add togglable behavior to card using @State to var isFaceUp (just for now,  it will be refactored) Remove duplicated code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -21,18 +21,21 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
+            let rectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(Color.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 3)
+                rectangle.foregroundColor(Color.white)
+                rectangle.strokeBorder(lineWidth: 3)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                rectangle
             }
-            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
