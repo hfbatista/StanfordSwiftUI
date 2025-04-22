56156fbd04e524d5416d9cc1107b266299a3e7c5
Henrique Batista <henriquefreitasbatista.devoteam@edp.com>
2025-04-22 02:04:18 +0100

    Initial Commit, creates the CardView and isFaceup logic

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
         }
+        .foregroundColor(Color.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(Color.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 3)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+        }
     }
 }
 
